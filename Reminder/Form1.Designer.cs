7b6ac5988e8e32284a60a6dcecbdf37d2f6805ad
MRTHAKER <mahek.haker@gmail.com>
2019-02-02 14:01:28 +0530

    Start Reading data from Database

--- Reminder/Form1.Designer.cs
+++ Reminder/Form1.Designer.cs
@@ -31,8 +31,6 @@
             this.components = new System.ComponentModel.Container();
             this.label2 = new System.Windows.Forms.Label();
             this.panel1 = new System.Windows.Forms.Panel();
-            this.label8 = new System.Windows.Forms.Label();
-            this.time = new System.Windows.Forms.DateTimePicker();
             this.button2 = new System.Windows.Forms.Button();
             this.button1 = new System.Windows.Forms.Button();
             this.label7 = new System.Windows.Forms.Label();
@@ -40,10 +38,16 @@
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.label6 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
-            this.date = new System.Windows.Forms.DateTimePicker();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.comboBox3 = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
             this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -59,8 +63,14 @@
             // 
             // panel1
             // 
+            this.panel1.Controls.Add(this.label10);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.comboBox3);
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Controls.Add(this.label9);
             this.panel1.Controls.Add(this.label8);
-            this.panel1.Controls.Add(this.time);
+            this.panel1.Controls.Add(this.comboBox2);
+            this.panel1.Controls.Add(this.comboBox1);
             this.panel1.Controls.Add(this.button2);
             this.panel1.Controls.Add(this.button1);
             this.panel1.Controls.Add(this.label7);
@@ -68,32 +78,11 @@
             this.panel1.Controls.Add(this.textBox2);
             this.panel1.Controls.Add(this.label6);
             this.panel1.Controls.Add(this.label5);
-            this.panel1.Controls.Add(this.label4);
             this.panel1.Controls.Add(this.label3);
-            this.panel1.Controls.Add(this.date);
             this.panel1.Location = new System.Drawing.Point(46, 50);
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(339, 325);
             this.panel1.TabIndex = 1;
-            // 
-            // label8
-            // 
-            this.label8.AutoSize = true;
-            this.label8.Location = new System.Drawing.Point(44, 73);
-            this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(35, 13);
-            this.label8.TabIndex = 12;
-            this.label8.Text = "label8";
-            // 
-            // time
-            // 
-            this.time.Checked = false;
-            this.time.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
-            this.time.Location = new System.Drawing.Point(125, 48);
-            this.time.Name = "time";
-            this.time.ShowUpDown = true;
-            this.time.Size = new System.Drawing.Size(92, 20);
-            this.time.TabIndex = 11;
             // 
             // button2
             // 
@@ -162,16 +151,6 @@
             this.label5.Text = "Send Reminder Notification through Text-Message";
             this.label5.TextAlign = System.Drawing.ContentAlignment.TopRight;
             // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(21, 52);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(90, 17);
-            this.label4.TabIndex = 2;
-            this.label4.Text = "Select Time :";
-            // 
             // label3
             // 
             this.label3.AutoSize = true;
@@ -182,18 +161,157 @@
             this.label3.TabIndex = 1;
             this.label3.Text = "Choose Date :";
             // 
-            // date
-            // 
-            this.date.Format = System.Windows.Forms.DateTimePickerFormat.Short;
-            this.date.Location = new System.Drawing.Point(125, 15);
-            this.date.Name = "date";
-            this.date.Size = new System.Drawing.Size(154, 20);
-            this.date.TabIndex = 0;
-            this.date.ValueChanged += new System.EventHandler(this.date_ValueChanged);
-            // 
             // timer1
             // 
             this.timer1.Interval = 2000;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "00",
+            "01",
+            "02",
+            "03",
+            "04",
+            "05",
+            "06",
+            "07",
+            "08",
+            "09",
+            "10",
+            "11",
+            "12",
+            "13",
+            "14",
+            "15",
+            "16",
+            "17",
+            "18",
+            "19",
+            "20",
+            "21",
+            "22",
+            "23",
+            "24",
+            "25",
+            "26",
+            "27",
+            "28",
+            "29",
+            "30",
+            "31",
+            "32",
+            "33",
+            "34",
+            "35",
+            "36",
+            "37",
+            "38",
+            "39",
+            "40",
+            "41",
+            "42",
+            "43",
+            "44",
+            "45",
+            "46",
+            "47",
+            "48",
+            "49",
+            "50",
+            "51",
+            "52",
+            "53",
+            "54",
+            "55",
+            "56",
+            "57",
+            "58",
+            "59"});
+            this.comboBox1.Location = new System.Drawing.Point(154, 83);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(100, 21);
+            this.comboBox1.TabIndex = 14;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "01",
+            "02",
+            "03",
+            "04",
+            "05",
+            "06",
+            "07",
+            "08",
+            "09",
+            "10",
+            "11",
+            "12"});
+            this.comboBox2.Location = new System.Drawing.Point(24, 83);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(121, 21);
+            this.comboBox2.TabIndex = 15;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(21, 52);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(90, 17);
+            this.label8.TabIndex = 16;
+            this.label8.Text = "Select Hour :";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(151, 52);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(100, 17);
+            this.label9.TabIndex = 17;
+            this.label9.Text = "Select Minutes";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(154, 15);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 18;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged_1);
+            // 
+            // comboBox3
+            // 
+            this.comboBox3.FormattingEnabled = true;
+            this.comboBox3.Items.AddRange(new object[] {
+            "AM",
+            "PM"});
+            this.comboBox3.Location = new System.Drawing.Point(268, 83);
+            this.comboBox3.Name = "comboBox3";
+            this.comboBox3.Size = new System.Drawing.Size(61, 21);
+            this.comboBox3.TabIndex = 19;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(277, 52);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(52, 17);
+            this.label4.TabIndex = 20;
+            this.label4.Text = "AM/PM";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(32, 219);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(41, 13);
+            this.label10.TabIndex = 21;
+            this.label10.Text = "label10";
             // 
             // Form1
             // 
@@ -215,17 +333,21 @@
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Label label6;
         private System.Windows.Forms.Label label5;
-        private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.DateTimePicker date;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Label label7;
         private System.Windows.Forms.TextBox textBox3;
         private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.DateTimePicker time;
         private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label9;
         private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox comboBox3;
+        private System.Windows.Forms.Label label10;
 
     }
 }
--- Reminder/Form1.cs
+++ Reminder/Form1.cs
@@ -9,6 +9,7 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Diagnostics;
 using System.Threading;
+using System.Data.SqlClient;
 
 
 namespace Reminder
@@ -21,23 +22,19 @@ namespace Reminder
         public Form1()
         {
             InitializeComponent();
-            date.CustomFormat = "dd-mmm-yyyy";
-            time.CustomFormat = "hh:mm:ss tt";
+          
+            
             /*Thread tt = new Thread(
     () => check()
 );
             tt.Start();*/
+            }
 
-           
-        }
-
+         
         private void button2_Click(object sender, EventArgs e)
         {
-            date.ResetText();
-            time.ResetText();
-            textBox2.Text = "";
-            textBox3.Text = "";
-            //label8.Text = "";
+
+            check();
         }
         public DateTime getdate()
         {
@@ -50,9 +47,14 @@ namespace Reminder
 
         private void button1_Click(object sender, EventArgs e)
         {
-            
-            msgbox();
-
+            SqlConnection con = conn();
+            con.Open();
+            string q = "insert into rem values ('" + textBox1.Text + "','" + comboBox1.SelectedItem + "','" + comboBox2.SelectedItem + "','" + comboBox3.SelectedItem + "','" + textBox2.Text + "','" + textBox3.Text + "')";
+            SqlCommand cmd = new SqlCommand(q, con);
+            int k = cmd.ExecuteNonQuery();
+            con.Close();
+            if (k > 0) { label10.Text = "done"; }
+            else { label10.Text = "derp"; }
         }
         
         public void msgbox()
@@ -90,13 +92,60 @@ namespace Reminder
 
         public void check()
         {
+            SqlConnection con = conn();
+            con.Open();
+            SqlCommand cm = new SqlCommand("Select * from rem", con);
+            SqlDataReader dr = cm.ExecuteReader();
+            DataTable dataTable = new DataTable();
+            dataTable.Load(dr);
+            int i = dataTable.Rows.Count;
+            String[] id = new String[i];
+            String[] date = new String[i];
+            String[] hour = new String[i];
+            String[] min = new String[i];
+            String[] ap = new String[i];
+            String[] to = new String[i];
+            String[] msg = new String[i];
+            int k = 0;
+            foreach (DataRow row in dataTable.Rows)
+            {
+                
+                
+                id[k] = row["Id"].ToString();
+                date[k] = row["date"].ToString();
+                hour[k] = row["hour"].ToString();
+                min[k] = row["min"].ToString();
+                ap[k] = row["ap"].ToString();
+                to[k] = row["to"].ToString();
+                msg[k] = row["msg"].ToString();
+                k++;
+            
+                
+            }
 
+            MessageBox.Show("'" + id[0] + "''" + date[0] + "''" + hour[0] + "''" + min[0] + "''" + ap[0] + "''" + to[0] + "''" + msg[0] + "'");
 
         }
 
+        public SqlConnection conn()
+        {
+            string s = @"Data Source=(LocalDB)\v11.0;AttachDbFilename='C:\Users\Om\Documents\Visual Studio 2012\Project\Reminder\SequenceDiagram1.mdf'";
+            SqlConnection con = new SqlConnection(s);
+            return con;
+        }
         
 
         private void date_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged_1(object sender, EventArgs e)
         {
 
         }
